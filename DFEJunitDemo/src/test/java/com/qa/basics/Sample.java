e8011da146fc5f38a197be13e330f7c3d5a25d21
Stewart <stewartjumbe@gmail.com>
2021-10-26 13:01:52 +0100

    changed file location

---
+++ DFEJunitDemo/src/test/java/com/qa/basics/Sample.java
@@ -0,0 +1,20 @@
+package com.qa.basics;
+
+public class Sample {
+	
+	//Junit is our testing framework, its open source
+	
+	//We can use different annotations based on the Junit version
+	//Junit 5 			- Junit 4
+	//@BeforeAll		-@BeforeClass
+	//@AfterAll			-@AfterClass
+	//@BeforeEach		-@BeforeEach
+	//@Test				-@Test
+	//@Disable			-@Ignore // Can be used to ignore a section of code so that its not tested
+	
+	@Test
+	public void someTest() {
+		
+	}
+
+}
---
+++ DFEMavenDemo/src/main/java/com/qa/main/PrintHello.java
@@ -0,0 +1,10 @@
+package com.qa.main;
+
+public class PrintHello {
+
+
+	public static void sayHello(String name) {
+		System.out.println("Hello" + name);
+		
+	}
+}
